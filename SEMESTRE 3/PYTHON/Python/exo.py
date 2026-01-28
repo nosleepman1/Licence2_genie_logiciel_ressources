50dc1db37b16ce40edfc354a0c77884c9559dcd7
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-01-28 23:12:58 +0000

    DELETEZ PYTHON BASES

--- SEMESTRE 3/PYTHON/Python/exo.py
+++
@@ -1,5 +0,0 @@
-
-# Créez les fonctions suivantes qui travaillent avec des chaînes :
-# a) inverser(texte) : Inverse une chaîne
-# b) est_palindrome(texte) : Retourne True si le texte est identique à son inverse
-# c) plus_long_mot(phrase) : Retourne le mot le plus long d’une phrase
